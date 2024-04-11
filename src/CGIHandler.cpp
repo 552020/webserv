2bec256fe40f8e2fa77c95abb91a2355dc999a98
Leonard Mangallon <lmangall@c4b12c2.42berlin.de>
2024-04-11 21:25:31 +0200

    improved createArgvForExecve

--- src/CGIHandler.cpp
+++ src/CGIHandler.cpp
@@ -11,10 +11,34 @@ std::string CGIHandler::handleRequest(const HTTPRequest &request) {
 
     std::string cgiOutput = executeCGI(env);
 
-
     return cgiOutput;
 }
 
+//key1=value1&key2=value2&key3=value3 might not be directly passed as command-line arguments (i.e., in argv)
+//it migth  be passed to the script as part of the environment variables
+//=> what is passed as command-line arguments ? path to the script + the path to the file to process ?
+char** CGIHandler::createArgvForExecve(const Environment& env) {
+    std::vector<const char*> argv;
+
+    std::string scriptName = env.getVar("SCRIPT_NAME");
+    std::string pathTranslated = env.getVar("PATH_TRANSLATED");
+	std::string pathToFile = pathTranslated + scriptName;
+    // std::string queryString = env.getVar("QUERY_STRING");
+
+    argv.push_back(scriptName.c_str());
+    argv.push_back(pathTranslated.c_str());
+    // argv.push_back(queryString.c_str());
+    argv.push_back(NULL);
+
+    char** argvArray = new char*[argv.size()];
+    for (size_t i = 0; i < argv.size(); ++i) {
+        size_t len = strlen(argv[i]);
+        argvArray[i] = new char[len + 1];
+        strcpy(argvArray[i], argv[i]);
+    }
+
+    return argvArray;
+}
 
 std::string CGIHandler::executeCGI(const Environment &env) {
 	std::cout<<"------------------inside CGIHandler::executeCGI-------------------" << std::endl;
@@ -22,6 +46,7 @@ std::string CGIHandler::executeCGI(const Environment &env) {
     std::string cgiScriptPath = env.getVar("SCRIPT_NAME");
     // const char** argv = NULL;
 	const char* argv[0] = {cgiScriptPath.c_str()};
+	
 
 	
 	int pipeFD[2];
